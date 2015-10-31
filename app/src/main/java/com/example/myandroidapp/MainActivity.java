8a0be96dd6fbe9f23c043d0c121e91fdd14e5cd2
matt <no-reply@github.com>
2015-10-31 22:29:59 +1100

    add sendMessage method

--- app/src/main/java/com/example/myandroidapp/MainActivity.java
+++ app/src/main/java/com/example/myandroidapp/MainActivity.java
@@ -2,13 +2,8 @@ package com.example.myandroidapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public void sendMessage(View view){
+        Intent intent=new Intent(this,DisplayMessageActivity.class);
+        EditText userInput=(EditText)findViewById(R.id.edit_message);
+        String message=userInput.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
+    }
 }
